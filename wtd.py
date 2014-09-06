6e99a8115fda3c7fdb6b34c125f2f6a63679617b
Fernando <fernando.bugni@gmail.com>
2014-09-06 19:45:03 -0300

    arreglos

--- wtd.py
+++ wtd.py
@@ -3,7 +3,6 @@
 """Configuration of the site"""
 
 import os
-import jinja2
 import webapp2
 
 from google.appengine.api import users
@@ -15,37 +14,7 @@ from controller.item import ItemList
 from controller.item import ItemNew
 from controller.item import ItemDelete
 
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), './app')),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True,
-    variable_start_string= '((', 
-    variable_end_string= '))')
-
-class MainPage(webapp2.RequestHandler):
-    """MainPage"""
-
-    def get(self):
-        """Gets a user"""
-        user = users.get_current_user()
-
-        if user:
-
-            values = {
-                "user" : user
-            }
-
-            self.response.headers['Content-Type'] = 'text/html'
-            template = JINJA_ENVIRONMENT.get_template('/views/main.html')
-            self.response.write(template.render(values))
-            
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
-
 application = webapp2.WSGIApplication([
-    #Web
-    webapp2.Route('/', handler=MainPage),
-
     #API REST
     webapp2.Route('/project', handler=ProjectList),
     webapp2.Route('/project/new', handler=ProjectNew),
